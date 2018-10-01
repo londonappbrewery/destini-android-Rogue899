de10d8bc0d6be742c61764999e7110e1da5e6478
Rogue899 <tarek.roukos@lau.edu>
2018-10-01 14:07:25 +0300

    Destini app with music and exit/restart buttons

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,11 +1,28 @@
 package com.londonappbrewery.destini;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
+    private TextView mStoryTextView;
+    private Button mButtonTop;
+    private Button mButtonBottom;
+    private int mStoryIndex = 1;
+    private MediaPlayer mMediaPlayer;
 
 
     @Override
@@ -14,17 +31,128 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer = MediaPlayer.create(this, R.raw.darkshadow);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setLooping(true);
+        mMediaPlayer.start();
 
 
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
+        mButtonTop = (Button) findViewById(R.id.buttonTop);
+        mButtonBottom = (Button) findViewById(R.id.buttonBottom);
+
+
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex==1) {
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mButtonTop.setText(R.string.T3_Ans1);
+                    mButtonBottom.setText(R.string.T3_Ans2);
+                    mStoryIndex=3;
+                }
+                else if(mStoryIndex==2){
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mButtonTop.setText(R.string.T3_Ans1);
+                    mButtonBottom.setText(R.string.T3_Ans2);
+                    mStoryIndex=3;
+                }
+                else if(mStoryIndex==3){
+                    mStoryTextView.setText(R.string.T6_End);
+                    mButtonBottom.setText("Exit");
+                    mButtonTop.setText("Restart!");
+                    mStoryIndex = 6;
+                    //alertMessage();
+                    mButtonTop.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i=new Intent(getApplicationContext(), MainActivity.class);
+                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(i);
+                            mMediaPlayer.reset();
+                        }
+                    });
+                    mButtonBottom.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mMediaPlayer.stop();
+                            finish();
+                        }
+                    });
+                }
+            }
+        });
+
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex==1) {
+                    mStoryTextView.setText(R.string.T2_Story);
+                    mButtonTop.setText(R.string.T2_Ans1);
+                    mButtonBottom.setText(R.string.T2_Ans2);
+                    mStoryIndex=2;
+                }
+                else if(mStoryIndex==2){
+                    mStoryTextView.setText(R.string.T4_End);
+                    mButtonBottom.setVisibility(View.GONE);
+                    mButtonTop.setVisibility(View.GONE);
+                    mStoryIndex=4;
+                    alertMessage();
+                }
+                else if (mStoryIndex==3){
+                    mStoryTextView.setText(R.string.T5_End);
+                    mButtonBottom.setVisibility(View.GONE);
+                    mButtonTop.setVisibility(View.GONE);
+                    mStoryIndex=5;
+                    alertMessage();
+
+                }
+
+            }
+        });
 
 
 
+    }
+    private void alertMessage(){
+        if (mStoryIndex == 4 || mStoryIndex == 5 || mStoryIndex == 6) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Game Over");
+            alert.setCancelable(false);
+            alert.setMessage("Restart with different decisions ?");
+            alert.setPositiveButton("Restart", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent i=new Intent(getApplicationContext(), MainActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                    mMediaPlayer.reset();
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+                }
+            });
+            alert.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                    mMediaPlayer.stop();
+                }
+            });
+            alert.show();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMediaPlayer.pause();
 
+    }
 
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        mMediaPlayer.start()
+        ;
     }
 }
